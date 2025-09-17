dde039e6fcf49f115e58763ea2971418e40aab41
deluwarhoisn <babuhossen301@gmail.com>
2025-09-17 12:24:16 +0600

    six

--- src/Bastman.jsx
+++ src/Bastman.jsx
@@ -2,10 +2,27 @@ import { useState } from "react"
 
 export default function Batsman(){
     const [runs,setRuns] = useState(0)
+    const [sixes, setSixes] = useState(0);
+    const handleSingle=() => {
+        const updatedRuns = runs + 1;
+        setRuns(updatedRuns);
+    }
+    const handelSix=() =>{
+        const updatedRuns = runs + 6;
+        const updatedSixes = sixes + 1;
+        setSixes(updatedSixes);
+        setRuns(updatedRuns);
+    }
+
 return(
     <div>
         <h3>Player: Bangla Batsman</h3>
+        <p><small>six: {sixes}</small></p>
+        
         <h1>Score:{runs} </h1>
+        <button onClick={handleSingle}>single</button>
+        <button>four</button>
+        <button onClick={handelSix}>six</button>
     </div>
 )
 }
